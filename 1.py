f90e15c607210c48ba357c1c0f386c8597146fc7
Моисеев Михаил <allorakspam@gmail.com>
2023-11-06 21:41:06 +0300

    Task 1 done

--- 1.py
+++ 1.py
@@ -1,2 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 if __name__ == '__main__':
-    pass
+    street = np.array([80, 98, 75, 91, 78])
+    garage = np.array([100, 82, 105, 89, 102])
+
+    correlation = np.corrcoef(street, garage)[0, 1]
+    print(f'Кореляция равна {correlation}')
+
+    plt.grid(True)
+    plt.title('Диаграмма рассеяния', fontsize=20)
+    plt.xlabel("Улица")
+    plt.ylabel("Парковка")
+    plt.scatter(x=street, y=garage, marker='o', color='green')
+    plt.show()
+
+"""
+Вывод: коэффициент корреляции практически равен -1, следовательно присутствует сильная отрицательная корреляция переменных
+"""
